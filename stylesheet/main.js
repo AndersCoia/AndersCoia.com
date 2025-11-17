bc6a6be512234b1e91045535e2eed5606dd6c85c
Anders Coia <anders.coia@proton.me>
2025-11-17 19:23:46 +1100

    design changes and formatting

--- stylesheet/main.js
+++ stylesheet/main.js
@@ -33,6 +33,27 @@ class SiteNav extends HTMLElement {
 }
 
 customElements.define('site-nav', SiteNav)
+class HomeNav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <nav>
+            <input type="checkbox"/>
+                <span></span>
+                <span></span>
+                <span></span>
+                </label>
+            <ul>
+                <li class="nav-logo"><a href="/"><img src="images/icons/nav-logo-home.svg" alt="logo"></a></li>
+                <li><a href="projects">Projects</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="contact">Contact</a></li>
+            </ul>
+        </nav>
+        `
+    }
+}
+
+customElements.define('home-nav', HomeNav)
 
 class PortfolioNav extends HTMLElement {
     connectedCallback() {
@@ -44,7 +65,7 @@ class PortfolioNav extends HTMLElement {
                 <span></span>
                 </label>
             <ul>
-                <li class="nav-logo"><a href="/"><img src="/images/icons/nav-logo.svg" alt=""></a></li>
+                <li class="nav-logo"><a href="/"><img src="/images/icons/nav-logo.svg" alt="logo"></a></li>
                 <li><a href="/projects">Projects</a></li>
                 <li><a href="/about">About</a></li>
                 <li><a href="/contact">Contact</a></li>
